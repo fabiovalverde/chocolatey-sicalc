9b428da01783b937969dd2d883602bc1e53105ff
fabiovalverde <42446709+fabiovalverde@users.noreply.github.com>
2020-10-14 17:17:19 -0300

    v1

---
+++ sicalc/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'Sicalc*'
+  url          = 'http://www.receita.fazenda.gov.br/publico/programas/sicalc/Unico/setup.exe'
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
