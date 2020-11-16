ea6309711b7809bb7e10eed67e8f465845626be6
fabiovalverde <42446709+fabiovalverde@users.noreply.github.com>
2020-11-16 14:09:52 -0300

    updates
    v2

--- sicalc/tools/chocolateyinstall.ps1
+++ sicalc/tools/chocolateyinstall.ps1
@@ -4,6 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Sicalc*'
   url          = 'http://www.receita.fazenda.gov.br/publico/programas/sicalc/Unico/setup.exe'
+  checksum       = '2b6561d1de02d5c838f964cc177c67cb'
+  checksumType   = 'MD5'
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
